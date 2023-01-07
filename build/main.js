68918cd99ec550b2dffaebf8efb31a67bf0edd1d
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-01-07 19:03:18 +0200

    ...

---
+++ build/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("azaz"),console.log(5)})();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./css/style.css";
+
+const sum = (a, b) => a + b;
+
+console.log("azaz");
+console.log(sum(2, 3));
